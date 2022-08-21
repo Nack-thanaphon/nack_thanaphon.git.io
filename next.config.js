185522b095f8fa9173bfdfc62c4882ca279c7209
nackdev2535 <e21bvz@gmail.com>
2022-08-21 22:58:02 +0700

    de

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
 }
 module.exports = {
+  distDir: 'build',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
