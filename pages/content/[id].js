c764ede3b49d4c0a8250c0635ecf36d30f7442e1
nackdev2535 <e21bvz@gmail.com>
2022-08-21 23:20:37 +0700

    deploy php host

--- pages/content/[id].js
+++ pages/content/[id].js
@@ -1,96 +1,96 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import ContentCard from "../component/ContentCard";
-import Profile from "../component/Profile";
+// import Head from "next/head";
+// import Image from "next/image";
+// import Link from "next/link";
+// import ContentCard from "../component/ContentCard";
+// import Profile from "../component/Profile";
 
 
-function posts({ posts }) {
-  return (
-    <>
-      {/* <h1>{posts.data.attributes.title}</h1> */}
+// function posts({ posts }) {
+//   return (
+//     <>
+//       {/* <h1>{posts.data.attributes.title}</h1> */}
 
-      <div className=" mx-auto  h-full bg-blue-600 m-0 p-0">
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
-            integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
-            crossOrigin="anonymous"
-            referrerpolicy="no-referrer"
-          />
-        </Head>
-        <div className="grid sm:grid-cols-3 ">
-          <div className="sm:col-span-2 sm:m-5 ">
-            <div className="p-1">
-              <div className="shadow-lg rounded-sm bg-white  h-screen">
-                <div className="my-3 p-3">
-                  <div className="flex justify-between py-3 my-auto">
-                    <div className="my-auto">
-                      <p className="sm:text-5xl text-3xl font-bold">
-                        DEV<span className="text-blue-500">LOG.</span>
-                      </p>
-                    </div>
-                    <div className="flex">
-                      <Link href={"/"}>
-                        <a className="bg-gradient-to-r from-blue-500 to-blue-900 text-white  sm:p-2 p-1 rounded-md m-1">
-                          <i className="fas fa-clipboard-list"></i> Back
-                        </a>
-                      </Link>
-                      <Link href={"/สมัครงาน.pdf"}>
-                        <a className="bg-gradient-to-r from-blue-500 to-blue-900 text-white  sm:p-2 p-1 rounded-md m-1">
-                          <i className="fas fa-cloud-download"></i>{" "}
-                          Download(.pdf)
-                        </a>
-                      </Link>
-                    </div>
-                  </div>
-                  <hr />
-                  <ul className="py-5">
+//       <div className=" mx-auto  h-full bg-blue-600 m-0 p-0">
+//         <Head>
+//           <link
+//             rel="stylesheet"
+//             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+//             integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+//             crossOrigin="anonymous"
+//             referrerpolicy="no-referrer"
+//           />
+//         </Head>
+//         <div className="grid sm:grid-cols-3 ">
+//           <div className="sm:col-span-2 sm:m-5 ">
+//             <div className="p-1">
+//               <div className="shadow-lg rounded-sm bg-white  h-screen">
+//                 <div className="my-3 p-3">
+//                   <div className="flex justify-between py-3 my-auto">
+//                     <div className="my-auto">
+//                       <p className="sm:text-5xl text-3xl font-bold">
+//                         DEV<span className="text-blue-500">LOG.</span>
+//                       </p>
+//                     </div>
+//                     <div className="flex">
+//                       <Link href={"/"}>
+//                         <a className="bg-gradient-to-r from-blue-500 to-blue-900 text-white  sm:p-2 p-1 rounded-md m-1">
+//                           <i className="fas fa-clipboard-list"></i> Back
+//                         </a>
+//                       </Link>
+//                       <Link href={"/สมัครงาน.pdf"}>
+//                         <a className="bg-gradient-to-r from-blue-500 to-blue-900 text-white  sm:p-2 p-1 rounded-md m-1">
+//                           <i className="fas fa-cloud-download"></i>{" "}
+//                           Download(.pdf)
+//                         </a>
+//                       </Link>
+//                     </div>
+//                   </div>
+//                   <hr />
+//                   <ul className="py-5">
                    
-                    <h1>{posts.data.attributes.title}</h1>
+//                     <h1>{posts.data.attributes.title}</h1>
 
-                    <div>{posts.data.attributes.detail}</div>
+//                     <div>{posts.data.attributes.detail}</div>
                   
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <Profile />
-        </div>
-      </div>
-    </>
-  );
-}
-export default posts;
+//                   </ul>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           <Profile />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+// export default posts;
 
-export async function getStaticPaths() {
-  const resp = await fetch(`http://localhost:1337/api/products?populate=p_img`);
-  const data = await resp.json();
+// export async function getStaticPaths() {
+//   const resp = await fetch(`http://localhost:1337/api/products?populate=p_img`);
+//   const data = await resp.json();
 
-  const paths = data.data.map((posts) => {
-    return {
-      params: {
-        id: `${posts.id}`,
-      },
-    };
-  });
+//   const paths = data.data.map((posts) => {
+//     return {
+//       params: {
+//         id: `${posts.id}`,
+//       },
+//     };
+//   });
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const resp = await fetch(`http://localhost:1337/api/products/${params.id}`);
-  const data = await resp.json();
+// export async function getStaticProps(context) {
+//   const { params } = context;
+//   const resp = await fetch(`http://localhost:1337/api/products/${params.id}`);
+//   const data = await resp.json();
 
-  return {
-    props: {
-      posts: data,
-    },
-  };
-}
+//   return {
+//     props: {
+//       posts: data,
+//     },
+//   };
+// }
